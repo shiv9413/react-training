335f007164aaedd36be0635adcff636db8c51fcf
SHIV <cu.gagankashyap@gmail.com>
2024-05-28 12:30:15 +0530

    Trait added

--- app/Http/Controllers/InstallationController.php
+++ app/Http/Controllers/InstallationController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
+use App\Traits\FunctionTrait;
 
 class InstallationController extends Controller
 {
+    use FunctionTrait;
     public function startInstallation(Request $request){
         /*
           1.New Installation
@@ -16,11 +18,11 @@ class InstallationController extends Controller
 
         try {
             $validRequest = $this->vaildRequestFromShopify($request->all());
-            if($validRequest){
+            if($validRequest) {
                 $shop = $request->has('shop');
-                if($shop){
-                    $storeDetails = $this->getStoreByName($request->shop);
-                    if($storeDetails != null && $storeDetails != false){
+                if($shop) {
+                    $storeDetails = $this->getStoreByDomain($request->shop);
+                    if($storeDetails !== null && $storeDetails !== false) {
                         /* 
                            store record exist and now determine whether the access token vaild or not
                            if not then forword them to re-installation flow
@@ -29,18 +31,31 @@ class InstallationController extends Controller
 
                         $validAccessToken = $this->checkifAccessTokenIsValid($storeDetails);
 
-                        if($validAccessToken){
+                        if($validAccessToken) {
                             // Token is valid for Shopify API Call so redirect them to login page.
-                            
+                            print_r('Here is valid token part');exit;
                         } else {
                             // Token is not valid so redirect the user to the re-installation phase
+                            print_r('Here is not an valid token part');exit;
                         }
+                    } else {
+                        // New installation flow should be carried out.
+                        print_r('new installation begin here');exit;
                     }
-                } else throw new Exception("Shop paramter not present in app");
+                    
+                } else throw new Exception("Shop paramter not present in the request");
             } else throw new Exception("Request is not valid"); 
-        } catch(Exception $e){
+        } catch(Exception $e) {
             \Log::info($e->getMessage().' '.$e->getLine());
-            dd($e->getMessage());
+            dd($e->getMessage().' '.$e->getLine());
         }
+    }
+
+    private function vaildRequestFromShopify($request) {
+        return true;
+    }
+
+    private function checkifAccessTokenIsValid($storeDetails) {
+
     }
 }
