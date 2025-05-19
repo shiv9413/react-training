47f9cf20d49ea41d339cb499be26666534e6b9c3
Gagan-React-Dev <cu.gagankashyap@gmail.com>
2025-05-19 22:48:00 +0530

    First Component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import First from './components/19-05-2025/First';
 
 function App() {
   return (
     <div>
-
+      <First />
     </div>
     );
 }
---
+++ src/components/19-05-2025/First.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function First(){
+	return (
+		<div>
+			<h1>This is my first jsx file</h1>
+			<p>This is my first component of react</p>
+		</div>
+		);
+}
+
+export default First;
